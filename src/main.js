c6b7f401853a360b8664785b7764b017a6e72e90
DreaMemory2 <crystalstalight699@gmail.com>
2025-01-08 01:08:54 +0800

    udating

--- src/main.js
+++ src/main.js
@@ -1,23 +1,14 @@
-/* 等同于引入Vue.js文件 */
 import Vue from 'vue'
-/* 引入App组件(根组件) */
 import App from './App.vue'
-
-/* 关闭生产提示信息 */
 Vue.config.productionTip = false
+
+// 引入Vuex插件中的核心对象store
+import store from './vuex/store';
 
 new Vue({
   el : '#app',
-  /* 您正在使用Vue的仅运行时的版本，其中模板编译器不可用 */
-  /* 采用缺失模板编译器vue.js，目的减小体积
-      - Vue.js包括两块：Vue的核心、模板编译器(占用比33%)
-     使用webpack进行打包处理之后，模板编译器就没有存在的必要
-  */
-  /* 这个函数被调用的时候会自动传入过来一个参数：createElement
-      createElement用来创建元素
-  */
-  /* render(createElement) {
-    return createElement(App);
-  } */
-  render: createElement => createElement(App)
+  render: h => h(App),
+  // 全新的配置项: store
+  /* 加上这个配置项之后，vm以及所有的vc对象上都会多一个属性：$store */
+  store : store
 });
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,4 +1,4 @@
-// 
+// 引入Vue，因为需要使用Vuex插件的时候需要Vue
 import Vue from 'vue';
 // 引入Vuex插件
 import Vuex from 'vuex';
@@ -6,5 +6,20 @@ import Vuex from 'vuex';
 // 使用插件
 Vue.use(Vuex);
 
-// 创建store对象
-new Vue
+// 创建三个对象: actions对象、mutations对象、state对象
+const actions = {};
+const mutations = {};
+const state = {};
+
+// 创建store对象(这个store对象是vuex插件中最核心的对象)
+const store = new Vuex.Store({
+  // 它是一个负责执行某个行为的对象
+  actions : actions,
+  // 它是一个负责更新的对象
+  mutations : mutations,
+  // 它是一个状态对象
+  state : state
+});
+
+// 导出store对象(暴露对象)
+export default store;
