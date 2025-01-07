d0eb939d29c6ee79f4322a6acc4c08de9f3847ed
DreaMemory2 <crystalstalight699@gmail.com>
2025-01-08 01:45:56 +0800

    Updating

---
+++ src/components/AComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>数字：{{$store.state.number}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'AComponent',
+  data() {
+    return {
+      number : 0
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -7,9 +7,40 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 // 创建三个对象: actions对象、mutations对象、state对象
-const actions = {};
-const mutations = {};
-const state = {};
+const actions = {
+  // N多个action, 每一个action都是一个callback回调函数
+  /* 
+    在action这种回调函数中编写复杂业务逻辑, 或者说发送AJAX请求
+    context参数: context是vuex的上下文
+    value参数: 传过来的数据
+  */
+  addNumber(context, value) {
+    // 业务逻辑
+    value = value + 1;
+    // 业务逻辑处理完成之后，继续向下一个环节，轮到数据的更新
+    // 提交上下文环境
+    context.commit('ADD_NUMBER', value);
+  }
+
+  /* 其他action逻辑 */
+};
+const mutations = {
+  // N多个mutation, 每一个mutation都是一个callback回调函数
+  /* 
+    每一个mutation这个回调函数的作用就是: 更新state
+    只要state更新，因为是响应式的，所以页面就重新渲染
+    state: 更新对象
+    value: 上一节传过来的数据
+  */
+  ADD_NUMBER(state, value) {
+    state.number += value;
+  }
+};
+
+// 等同于Vue当中的data(状态)
+const state = {
+  number : 0
+};
 
 // 创建store对象(这个store对象是vuex插件中最核心的对象)
 const store = new Vuex.Store({
